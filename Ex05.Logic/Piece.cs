ae64310ff92945605ef6dddc9a608112699c5388
AvivJacobs <aviv23640579@gmail.com>
2019-04-15 14:50:33 +0300

    uploading checker game

---
+++ Ex05.Logic/Piece.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex05.Logic
+{
+    public class Piece
+    {
+        private BoardPosition m_Position;
+        private char m_PieceSign;
+
+        public enum ePieceType
+        {
+            Men,
+            King
+        }
+
+        private ePieceType m_PieceType;
+        private bool m_IsPieceStillActive;
+
+        public BoardPosition Position
+        {
+            get { return m_Position; }
+            set { m_Position = value; }
+        }
+
+        public ePieceType PieceType
+        {
+            get { return m_PieceType; }
+            set { m_PieceType = value; }
+        }
+
+        public char PieceSign
+        {
+            get { return m_PieceSign; }
+            set { m_PieceSign = value; }
+        }
+
+        public bool IsPieceStillActive
+        {
+            get { return m_IsPieceStillActive; }
+            set { m_IsPieceStillActive = value; }
+        }
+    }
+}
---
+++ Ex05.UI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B18_Ex05
+{
+    // $G$ RUL-005 (-10) Wrong zip folder name: 2 spaces between first student and his id
+    public class Program
+    {
+        // $G$ SFN-013 (+18) Bonus: UI with richer graphics / motion / sound.
+
+        public static void Main()
+        {
+            UserInteraction UI = new UserInteraction();
+            UI.RunGame();
+        }
+    }
+}
